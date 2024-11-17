036444cc52bfe426afa97ed503734091e33e12fc
ARKA BASAK <sepic466@gmail.com>
2024-11-18 01:18:11 +0530

    random image

--- api/index.js
+++ api/index.js
@@ -48,19 +48,37 @@ app.post('/proxy/mangadex-cover', async (req, res) => {
   });
 
 
-  app.post("/api/v1/chapter-read",async(req,res)=>{
-    try {
-      const {chapId} = req.body
-      const resp = await axios.get (
-        `https://api.mangadex.org/at-home/server/${chapId}`
-      )
-      console.log(resp.data)
-      return res.status(200).send({message: resp.data})    
-    } catch (error) {
-        return res.status(400).send({message:"error while fetching image", error})
-    }
-  })
+app.post("/api/v1/chapter-read",async(req,res)=>{
+  try {
+    const {chapId} = req.body
+    const resp = await axios.get (
+      `https://api.mangadex.org/at-home/server/${chapId}`
+    )
+    console.log(resp.data)
+    return res.status(200).send({message: resp.data})    
+  } catch (error) {
+      return res.status(400).send({message:"error while fetching image", error})
+  }
+})
   
+
+app.get("/proxy/random", async (req, res) => {
+  try {
+    const coverUrl = `https://pic.re/images`;
+    const imageResponse = await axios.get(coverUrl, {
+      responseType: 'arraybuffer',
+      // headers: { 'Referer': 'https://mangadex.org' },
+    });
+
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*');  // Allow any origin to access this resource
+    res.setHeader('Content-Type', imageResponse.headers['content-type']);  // Set the correct image content type
+    res.send(imageResponse.data);  // Send image data as response
+  } catch (error) {
+    console.error('Error fetching image:', error);
+    res.status(500).send('Error fetching image');
+  }
+});
 
 app.listen(3001, () => console.log("Server ready on port 3001."));
 
